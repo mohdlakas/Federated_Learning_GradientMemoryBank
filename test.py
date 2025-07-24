228c2f9568272eca9d22f091dc652c08815a462f
Mohamed Lakas <66885063+mohdlakas@users.noreply.github.com>
2025-07-24 16:06:53 +0400

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torchvision.datasets as datasets
-
-# This will show you where MNIST gets downloaded
-train_dataset = datasets.MNIST(root='./data', train=True, download=True)
-print(f"Dataset root: {train_dataset.root}")
